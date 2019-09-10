e89131e9610ccd1793ef80fe352de6be7b198393
liangqiankun <liangqiankun@DESKTOP-LPQJ9OL>
2019-09-10 12:48:06 +0800

    日历类设置时间

---
+++ src/com/lqk/demo/CalendarDemo.java
@@ -0,0 +1,30 @@
+package com.lqk.demo;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class CalendarDemo {
+	public static void main(String[] args) {
+		String inputDate = "2019/09/10";
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		Calendar calendar = Calendar.getInstance();
+		try {
+			calendar.setTime(dateFormat.parse(inputDate));
+			
+			calendar.add(Calendar.YEAR, 1);
+			// 2020.09.10
+			System.out.println(dateFormat.format(calendar.getTime()));
+			
+			calendar.add(Calendar.MONTH, 1);
+			// 2020.10.10
+			System.out.println(dateFormat.format(calendar.getTime()));
+			
+			calendar.add(Calendar.DATE, 1);
+			// 2020.10.11
+			System.out.println(dateFormat.format(calendar.getTime()));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
