6d6d216fb96fbde904f44f6ee0f2d95faa1812f6
liangqiankun <liangqiankun@DESKTOP-LPQJ9OL>
2019-09-24 17:17:57 +0800

    map

--- src/com/lqk/demo/MapDemo.java
+++ src/com/lqk/demo/MapDemo.java
@@ -26,26 +26,26 @@ public class MapDemo {
 			System.out.println("key:" + key + ",value:" + map.get(key));
 		}
 		
-		System.out.println("------------------获取map种所有的value：map.values()--------------------------");
+		System.out.println("------ 获取map种所有的value：map.values() ------");
 		//遍历所有的value
 		for(String value : map.values()){
 			System.out.println("value:"+ value);
 		}
 		
-		System.out.println("-------------------获取键值对：map.entrySet()-------------------------");
+		System.out.println("------ 获取键值对：map.entrySet() ------");
 		//取出对应的 key，value 键值对,容量大时推荐使用
 		for(Map.Entry<String, String> entry : map.entrySet()){
 			System.out.println("键值对：" + entry);
 			System.out.println("key:" + entry.getKey());
 			System.out.println("value:" + entry.getValue());
 		}
-		System.out.println("--------------------- 通过 Map.entrySet使用iterator遍历 key 和 value -----------------------");
+		System.out.println("------ 通过 Map.entrySet使用iterator遍历 key 和 value ------");
 		Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
 		while(iterator.hasNext()){
 			Map.Entry<String, String> entry = iterator.next();
 			System.out.println("key:" + entry.getKey() + ",value:" + entry.getValue());
 		}
-		System.out.println("-------------------- map.forEach JDK1.8 新特性 ----------------------");
+		System.out.println("------ map.forEach JDK1.8 新特性 ------");
 		
 		map.forEach((key,value)->{
 			System.out.println("key=" +key+",value ="+value);
